c535da9c1d47db5ddbe9a7f497a2409cba33d169
pigey <pigfof@gmail.com>
2022-02-11 10:06:07 +0100

    min första webbapp

---
+++ index.js
@@ -0,0 +1,19 @@
+const cors = require("cors")
+const express = require("express")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use('/healthcheck', require('./routes/healthcheck.routes'));
+app.use(express.urlencoded({ extended: true}));
+app.use(cors())
+
+app.get("/",(req ,res)=>{
+    headers ={"http_status":200, "cache-control": "no-cache"}
+    body={"status": "available"}
+    res.status(200).send(body)
+})
+
+app.listen(PORT , ()=>{
+    console.log('STARTED LISTENING ON PORT ${PORT}')
+});
