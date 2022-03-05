893352b0d39d907ed7fe0ee29caa8ca14a1651b7
pigey <pigfof@gmail.com>
2022-03-05 11:32:15 +0100

    first push

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const cors = require("cors")
 const express = require("express")
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
+
+let users = {}
 
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded({ extended: true}));
@@ -14,6 +16,21 @@ app.get("/",(req ,res)=>{
     res.status(200).send(body)
 })
 
+app.get("/register", (req, res) => {
+    let user = req.query.user
+    let time = req.query.time
+    console.log(user)
+    console.log(time)
+    users[user] = time
+    res.status(200).send({"status":"success"})
+})
+
+app.get("/highscores", (req, res) => {
+    res.status(200).send(users)
+})
+
+
+
 app.listen(PORT , ()=>{
-    console.log('STARTED LISTENING ON PORT ${PORT}')
+    console.log(`STARTED LISTENING ON PORT ${PORT}`)
 });
